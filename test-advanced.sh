33b7b768b3fcc419611b440144f57956f02a77a7
Jim Koning <jkoning@pixsystem.com>
2014-03-02 15:06:58 -0800

    hls test scripts

---
+++ test-advanced.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./ffplay https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8 &
---
+++ test-basic.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./ffplay https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8 &
