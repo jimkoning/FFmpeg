fa939add0a504387e4cb90d96e2dff78c0b94b07
Jim Koning <jkoning@pixsystem.com>
2014-04-02 14:07:37 -0700

    lint

--- ffplay.c
+++ ffplay.c
@@ -3162,7 +3162,7 @@ static void refresh_loop_wait_event(VideoState *is, SDL_Event *event) {
             cursor_hidden = 1;
         }
         if (remaining_time > 0.0)
-            av_usleep((int64_t)(remaining_time * 1000000.0));
+            av_usleep(remaining_time * 1000000.0);
         remaining_time = REFRESH_RATE;
         if (is->show_mode != SHOW_MODE_NONE && (!is->paused || is->force_refresh))
             video_refresh(is, &remaining_time);
